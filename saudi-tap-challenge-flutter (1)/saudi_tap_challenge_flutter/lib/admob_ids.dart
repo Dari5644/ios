06ae647f3532ac97142320df3b055ecff8fbf92f
Dari5644 <majddary1345@gmail.com>
2026-03-02 04:32:44 +0300

    Add files via upload

---
+++ saudi-tap-challenge-flutter (1)/saudi_tap_challenge_flutter/lib/admob_ids.dart
@@ -0,0 +1,6 @@
+class AdmobIds {
+  // ضع معرفاتك الحقيقية هنا من AdMob
+  static const String iosAppId = "ca-app-pub-XXXXXXXXXXXXXXXX~YYYYYYYYYY";
+  static const String bannerUnitId = "ca-app-pub-XXXXXXXXXXXXXXXX/BBBBBBBBBB";
+  static const String rewardedUnitId = "ca-app-pub-XXXXXXXXXXXXXXXX/RRRRRRRRRR";
+}
