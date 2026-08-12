51a747f46adbeb79ffba07169ea27a98b92cf650
Dari5644 <majddary1345@gmail.com>
2026-08-12 05:46:16 +0300

    Delete backend directory

--- backend/src/routes/classes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const PlayerClass = require('../models/PlayerClass');
-
-// عام - لعرض الكلاسات في صفحة "الكلاّس"
-router.get('/', (req, res) => res.json(PlayerClass.all()));
-
-module.exports = router;
--- backend/src/routes/rules.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Rule = require('../models/Rule');
-
-router.get('/', (req, res) => res.json(Rule.allTabsWithItems()));
-
-module.exports = router;
