eb2dd86aa07737705a76f13306e53463ee7234ad
Dari5644 <majddary1345@gmail.com>
2026-08-12 05:46:37 +0300

    Delete bot directory

--- bot/events/guildMemberAdd.js
+++
@@ -1,9 +0,0 @@
-const db = require('../utils/db');
-
-module.exports = {
-  name: 'guildMemberAdd',
-  execute(member) {
-    db.prepare('INSERT INTO activity_log (type, message) VALUES (?, ?)')
-      .run('join', `${member.user.username} انضم إلى سيرفر الديسكورد 🎮`);
-  }
-};
